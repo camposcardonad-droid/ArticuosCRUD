06dce57280846366ec1a26a310f451efe1a24dda
Dominic Campos <camposcardonad@gmail.com>
2026-08-27 11:18:51 -0600

    Menu estatico

---
+++ Menu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArticuosCRUD
+{
+    internal class Menu
+    {
+        public Menu()
+        {
+
+        }
+        public void MostrarMenu()
+        {
+            bool continuar = true;
+            while (continuar)
+            {
+                Console.Clear();
+                Console.WriteLine("Gestor de Articulos");
+                Console.WriteLine("====================");
+                Console.WriteLine("1. Agregar");
+                Console.WriteLine("2. Listar");
+                Console.WriteLine("3. Buscar");
+                Console.WriteLine("4. Modificar");
+                Console.WriteLine("5. Eliminar");
+                Console.WriteLine("6. Salir");
+                string opcion = Console.ReadLine() ?? "";
+                switch(opcion)
+                {
+                    case "0":
+                        continuar = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opcion Invalida");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
+    }
+}
