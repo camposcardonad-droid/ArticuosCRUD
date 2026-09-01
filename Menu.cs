972d2d69012fafef9dab7a531b72e6b7c56bf8ae
Dominic Campos <camposcardonad@gmail.com>
2026-09-01 11:16:12 -0600

    Menu y clase producto

--- Menu.cs
+++ Menu.cs
@@ -6,9 +6,12 @@ namespace ArticuosCRUD
 {
     internal class Menu
     {
-        public Menu()
+        private readonly string Titulo;
+        private readonly string[] Opciones;
+        public Menu(string titulo, string[] opciones)
         {
-
+            Titulo = titulo;
+            Opciones = opciones;
         }
         public void MostrarMenu()
         {
@@ -16,14 +19,21 @@ namespace ArticuosCRUD
             while (continuar)
             {
                 Console.Clear();
-                Console.WriteLine("Gestor de Articulos");
-                Console.WriteLine("====================");
-                Console.WriteLine("1. Agregar");
-                Console.WriteLine("2. Listar");
-                Console.WriteLine("3. Buscar");
-                Console.WriteLine("4. Modificar");
-                Console.WriteLine("5. Eliminar");
+                Console.WriteLine(Titulo);
+                Console.WriteLine(new string('#', Titulo.Length));
+                for (int i = 0; i < Opciones.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1}, {Opciones[i]}");
+                }
                 Console.WriteLine("0. Salir");
+                //Console.WriteLine("Gestor de Articulos");
+                //Console.WriteLine("====================");
+                //Console.WriteLine("1. Agregar");
+                //Console.WriteLine("2. Listar");
+                //Console.WriteLine("3. Buscar");
+                //Console.WriteLine("4. Modificar");
+                //Console.WriteLine("5. Eliminar");
+                //Console.WriteLine("0. Salir");
                 string opcion = Console.ReadLine() ?? "";
                 switch(opcion)
                 {
